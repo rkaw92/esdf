85dadb2f0fcd3a37d9acacf12b83df26fbc2ae43
Robert Kawecki <thewanderer@phobos.rkaw.pl>
2013-06-12 17:14:21 +0200

    AR command deduplication now works on a per-commit basis. The wrapper now also supports relaying dummy values back to the caller.

--- EventSourcedAggregate.js
+++ EventSourcedAggregate.js
@@ -64,14 +64,14 @@ EventSourcedAggregate.prototype.assignEventSink = function assignEventSink(event
  * 
  * @param {Array} event_array The array of events to apply. Each array element must be an object with at least EventType and EventObject properties, which will be used when applying.
  */
-EventSourcedAggregate.prototype.applyCommit = function applyCommit(event_array){
+EventSourcedAggregate.prototype.applyCommit = function applyCommit(event_array, command_id){
 	for(var i = 0; i < event_array.length; i++){
 		this.apply(event_array[i].EventType, event_array[i].EventObject);
-		//If the event carries a CommandID tag...
-		if(typeof(event_array[i].CommandID) === 'string'){
-			//Flag the command as already executed.
-			this._executedCommands[event_array[i].CommandID] = true;
-		}
+	}
+	//If the event carries a CommandID tag...
+	if(typeof(command_id) === 'string'){
+		//Flag the command as already executed.
+		this._executedCommands[command_id] = true;
 	}
 	this.nextSequenceNumber++;
 };
@@ -96,20 +96,11 @@ EventSourcedAggregate.prototype.apply = function apply(event_name, event_object)
  * @param {string} event_type Type of the event to apply. This is the name used both for routing the event to local listeners (including the Aggregate's own) and saving it to the Event Sink.
  * @param {Object} event_params The event payload to pass to local listeners and the Event Sink.
  */
-EventSourcedAggregate.prototype.stage = function stage(event_type, event_params, command_id){
-	//Make sure that only string command IDs are saved.
-	if(typeof(command_id) === 'number'){
-		command_id = command_id + '';
-	}
-	if(typeof(command_id) === 'string'){
-		//If we have a command ID, register the command as already executed.
-		this._executedCommands[command_id] = true;
-	}
+EventSourcedAggregate.prototype.stage = function stage(event_type, event_params){
 	this.stagedEvents.push({
 		EventID: uuid.v4(),
 		EventType: event_type,
-		EventObject: event_params,
-		CommandID: command_id
+		EventObject: event_params
 	});
 	this.apply(event_type, event_params);
 	return true;
@@ -122,52 +113,66 @@ EventSourcedAggregate.prototype.stage = function stage(event_type, event_params,
  * 
  * @returns {Promise} Promise/A-compliant promise object which supports then(). The promise is resolved when the event is saved, and rejected if the saving fails for any reason (including optimistic concurrency).
  */
-EventSourcedAggregate.prototype.commit = function commit(){
+EventSourcedAggregate.prototype.commit = function commit(command_id){
 	var self = this;
 	var emit_deferred = when.defer(); //emission promise - to be resolved when the event batch is saved in the database
-	//Guard clause - if no events staged, do not litter the Event Store (especially useful with the idempotency wrapper deduplicateMethodCall).
+	//Guard clause: if no events staged, do not litter the Event Store (especially useful with the idempotency wrapper deduplicateMethodCall).
 	if(self.stagedEvents.length < 1){
 		return emit_deferred.resolver.resolve(true);
 	}
+	//Guard clause: if this command duplicate, there should have been no events staged.
+	// However, if the programmer forgot to include deduplication in the respective methods, we want to avoid a commit at all costs.
 	when(self.eventSink.sink(self.stagedEvents, self.aggregateID, self.nextSequenceNumber),
 	function _eventSinkSucceededucceeded(result){
+		//If the commit carries a command ID tag, flag the command as 
+		if(typeof(command_id) === 'string'){
+			//Flag the command as already executed internally.
+			self._executedCommands[command_id] = true;
+		}
 		self.nextSequenceNumber++;
 		return emit_deferred.resolver.resolve(true);
 	},
 	function _eventSinkFailed(reason){
 		setImmediate(function(){
-			self.apply('error', reason);
+			//TODO: eradicate this completely or find another clean solution
+			//self.apply('error', reason);
 		});
 		return emit_deferred.resolver.reject(reason);
-	}); //this is a promise (thenable), so return its consumer-meant part
+	}); //This is a promise (thenable), so return its consumer-meant part.
 	return emit_deferred.promise;
 };
 
 /**
- * Wrap a method in a deduplication layer. The layer ensures that duplicate method calls (recognized via a supplied command ID, always the first argument) will always return the same value.
+ * Wrap a method in a deduplication layer. The layer ensures that duplicate method calls (recognized via the supplied command ID) will not have any effect.
  * 
+ * @param {function} method The AR method to wrap. It is not necessary to bind its "this" to the object in any way.
+ * @param {function} [default_value_constructor] The constructor for the dummy value that is returned when a command duplicate is detected. Mostly useful for asynchronous methods - a dummy promise should be returned by the value constructor.
  * @returns {methodCallDeduplicationWrapper} The call deduplication wrapper function.
  */
-EventSourcedAggregate.deduplicateCommand = function deduplicateCommand(method){
-	return function methodCallDeduplicationWrapper(command_id){
-		//Turn the arguments into a proper array.
+EventSourcedAggregate.deduplicateCommand = function deduplicateCommand(method, default_value_constructor){
+	if(!default_value_constructor){
+		default_value_constructor = function(){}; // Returns undefined.
+	}
+	
+	/**
+	 * Takes a command ID, and possibly other arguments, and executes the wrapped method if not duplicate. If duplicate, returns the default value defined at wrapper creation time.
+	 * 
+	 * @typedef methodCallDeduplicationWrapper
+	 * @function
+	 * @param {string} command_id The command ID for possible deduplication.
+	 */
+	var methodCallDeduplicationWrapper = function methodCallDeduplicationWrapper(command_id){
+		//Check for duplicates.
 		if(!this._executedCommands[command_id]){
-			//We have not seen this command ID in the past, so issue the command as usual. In case it does turn out to be a duplicate anyway (because our state was not yet updated), we have optimistic concurrency to help us.
-			method.apply(this, arguments);
-			//Commands may never return any values, so explicitly return undefined.
-			return;
+			//We have not seen this command ID in the past, so issue the command as usual.
+			//In case it does turn out to be a duplicate anyway (because our state was not yet updated), we have optimistic concurrency to help us.
+			return method.apply(this, arguments);
 		}
 		else{
-			return;
+			return default_value_constructor();
 		}
 	};
+	return methodCallDeduplicationWrapper;
 };
-/**
- * Takes a command ID, and possibly other arguments, and executes the wrapped method.
- * 
- * @name methodCallDeduplicationWrapper
- * @function
- * @param {string} command_id
- */
 
 module.exports.EventSourcedAggregate = EventSourcedAggregate;
