ac162fd1afdc387b87b3325910d3f4554b714c6f
Robert Kawecki <robert@geo-com.pl>
2013-12-04 13:25:33 +0100

    Export missing EventBus components from the index.

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ module.exports.utils = require('./utils');
 module.exports.test = {};
 module.exports.test.DummyEventSink = require('./EventStore/DummyEventSink.js').DummyEventSink;
 module.exports.test.DummyEventSinkStreamer = require('./EventStore/DummyEventSinkStreamer.js').DummyEventSinkStreamer;
+module.exports.test.DummyEventBusPublisher = require('./EventBus/DummyEventBusPublisher.js').DummyEventBusPublisher;
+module.exports.test.DummyEventBusSubscriber = require('./EventBus/DummyEventBusSubscriber.js').DummyEventBusSubscriber;
 module.exports.test.DummyAggregateSnapshotter = require('./EventStore/DummyAggregateSnapshotter.js').DummyAggregateSnapshotter;
